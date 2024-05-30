9027ea78081c9347a85d74c3a671e26abdfc4cd3
Tharaniesh <99646275+Tharaniesh3@users.noreply.github.com>
2024-05-31 00:43:29 +0530

    30th commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,6 @@ import { type ClassValue, clsx } from 'clsx'
 
 import { twMerge } from 'tailwind-merge'
 import qs from 'query-string'
-
 import { UrlQueryParams, RemoveUrlQueryParams } from '@/types'
 
 export function cn(...inputs: ClassValue[]) {
